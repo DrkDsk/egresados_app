ac7c5c8415a1b1d6a17b6a9c44c2d0e55f13d0e0
Alfredo Palacios <joseaph.1998@gmail.com>
2021-01-06 01:49:05 -0600

    Se modifica Diseño de la vista Tramites.dart

--- lib/pages/Tramites.dart
+++ lib/pages/Tramites.dart
@@ -4,9 +4,10 @@ import 'package:app_egresados/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'Formulario.dart';
 import 'MyDrawer.dart';
 import 'package:http/http.dart' as http;
+
+import 'Registro.dart';
 
 class Tramite extends StatefulWidget {
   @override
@@ -24,21 +25,61 @@ class _TramiteView extends State<Tramite>{
   bool isLoading = true;
   bool sinTramites = false;
   String mensaje = "";
+  double topContainer = 0;
 
   getTramites() async {
 
+    setState(() {isLoading = true;});
+    List<dynamic> responseList = [];
+    List<Widget> listItems = [];
+
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     String id = sharedPreferences.getInt('id').toString();
-
-    setState(() {isLoading = true;});
 
     try{
       var tramites = await http.get("http://192.168.1.68:8000/api/getTramites/"+id);
       if(tramites.statusCode == 200){
         setState(() {
-          listTramites = json.decode(tramites.body);
+          responseList = json.decode(tramites.body);
           isLoading = false;
         });
+
+        responseList.forEach((tramite) {
+          listItems.add(Container(
+            height: 120,
+            margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15),
+            decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20.0)),color: Colors.white, boxShadow: [
+              BoxShadow(color: Colors.black.withAlpha(100),blurRadius: 10.0),
+            ]),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tramite["name"],
+                        style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 5),
+                      RaisedButton(
+                        child: Text("Solicitar"),
+                        onPressed: (){
+                            setState(() {isLoading = true;});
+                            setTramite(tramite["name"]);
+                        }
+                      ),
+                      SizedBox(height: 10)
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ));
+        });
+        setState(() {listTramites = listItems;});
       }
       else if(tramites.statusCode == 401){
         setState(() {
@@ -87,38 +128,37 @@ class _TramiteView extends State<Tramite>{
             else
               noTramites("No tienes trámites pendientes")
           ],
-        )) : ListView.separated(
-          padding: const EdgeInsets.all(8),
+        )) : ListView.builder(
+          padding: const EdgeInsets.all(6),
           itemCount: listTramites.length,
           itemBuilder: (BuildContext context,int index){
-            return Container(
-              height: 100,
-              child: Center(child: Column(
-                children: [
-                  Text("${listTramites[index]["name"]}",style: TextStyle(
-                    fontSize: 18,
-                  )),
-                  RaisedButton(
-                    color: Colors.blue,
-                    onPressed: (){
-                      setState(() {isLoading = true;});
-                      setTramite(listTramites[index]["name"]);
-                    },
-                    child: Text("Solicitar")
-                  ),
-                  Text(mensaje)
-                ],
-              )),
+            double scale = 1.0;
+            if (topContainer > 0.5) {
+              scale = index + 0.5 - topContainer;
+              if (scale < 0) {
+                scale = 0;
+              } else if (scale > 1) {
+                scale = 1;
+              }
+            }
+            return Opacity(
+              opacity: scale,
+              child: Transform(
+                transform: Matrix4.identity()..scale(scale,scale),
+                alignment: Alignment.bottomCenter,
+                child: Align(
+                  heightFactor: 0.9,
+                  alignment: Alignment.topCenter,
+                  child: listTramites[index],
+                ),
+              ),
             );
-          },
-          separatorBuilder: (BuildContext context, int index) => const Divider(),
+          }
         )
       ),
     );
   }
 }
-
-
 
 Container noTramites(String registrado){
   return Container(
