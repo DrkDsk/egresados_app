b132acd4797d5ec2cc16d765d065a29b8abfa630
Alfredo Palacios <joseaph.1998@gmail.com>
2021-01-25 01:40:07 -0600

    se importa el container Welcome.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:app_egresados/pages/MyDrawer.dart';
-import 'package:app_egresados/pages/Registro.dart';
-import 'package:app_egresados/pages/login.dart';
+import 'package:app_egresados/pages/Login.dart';
+import 'package:app_egresados/widgets/Welcome.dart';
 import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() {runApp(MyApp());}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,7 +32,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   SharedPreferences sharedPreferences;
-
   @override
   void initState() {
     super.initState();
@@ -43,10 +40,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
   checkLogin() async {
     sharedPreferences = await SharedPreferences.getInstance();
-    if (sharedPreferences.getString('token') == null)
+    var token = sharedPreferences.getString("token");
+    if(token != null){
+      if (JwtDecoder.isExpired(token)){
+        sharedPreferences.clear();
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
+                (Route<dynamic> route) => false);
+      }
+    }
+    else{
       Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
               (Route<dynamic> route) => false);
+    }
   }
 
   GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
@@ -58,30 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Registro de Titulación de Egresados',
             style: TextStyle(color: Colors.white)),
       ),
-      body: Container(
-        padding: EdgeInsets.only(left: 25),
-        child: ListView(
-          children: [
-            header(),
-            Padding(padding: EdgeInsets.only(bottom: 40)),
-            Text('Bienvenido a la Aplicación de \nRegistro de Titulación de Egresados',style:
-              TextStyle(color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 22)),
-            Padding(padding: EdgeInsets.only(bottom: 60)),
-            MaterialButton(onPressed: (){_drawerKey.currentState.openDrawer();},
-              child: Text('1) Registrate en la Sección Formulario para Actualizar tu información',style: TextStyle(
-                color: Colors.teal[300],fontWeight: FontWeight.bold, fontSize: 20))),
-            Divider(height: 60),
-            MaterialButton(onPressed: (){_drawerKey.currentState.openDrawer();},
-                child: Text('2) Dirígite a la Sección Trámites para conocer tus Trámites Disponibles',style: TextStyle(
-                    color: Colors.teal[300],fontWeight: FontWeight.bold, fontSize: 20))),
-            Divider(height: 60,),
-            MaterialButton(onPressed: (){_drawerKey.currentState.openDrawer();},
-                child: Text('3) Dirígite a la Sección Citas para conocer las Citas que tienes de Cada Trámite Solicitado',style: TextStyle(
-                    color: Colors.teal[300],fontWeight: FontWeight.bold, fontSize: 20))),
-          ],
-        ),
-      )
-      ,
+      body: Welcome(),
       drawer: MyDrawer(),
     );
   }
